957346f7c78d2b071d6f94986e19fe8a706f7e3b
hamzacos <hamza.laqraa@hotmail.com>
2021-11-11 14:05:24 +0100

    Done

---
+++ semain1/challange13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ char a;
+ printf("entre un nombre\t");
+ scanf("%s",&a);
+ printf("le nombre en decimale est:%d\n",a);
+ printf("le nombre en hexadicimale est:%x\n",a);
+ printf("le nombre en octale est:%o\n",a);
+ return(0);
+
+}
